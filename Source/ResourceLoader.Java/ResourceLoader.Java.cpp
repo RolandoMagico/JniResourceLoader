ee524764268849ff388d1d838c6598e4d533dae2
RolandoMagico <75312877+RolandoMagico@users.noreply.github.com>
2020-12-19 08:31:51 +0100

    Build with empty implementation working

--- Source/ResourceLoader.Java/ResourceLoader.Java.cpp
+++ Source/ResourceLoader.Java/ResourceLoader.Java.cpp
@@ -25,7 +25,7 @@
 /***********************************************************************************************************************
  INCLUDES
 ***********************************************************************************************************************/
-#include "rolandomagico_jniregistry_Registry.h"
+#include "rolandomagico_jniresourceloader_ResourceLoader.h"
 #include <windows.h>
 
 /***********************************************************************************************************************
@@ -51,71 +51,11 @@
 /***********************************************************************************************************************
  IMPLEMENTATION
 ***********************************************************************************************************************/
-LSTATUS ReadRegistry(JNIEnv* env, jint hKey, jstring location, jstring key, PVOID buffer, LPDWORD bufferLength)
+JNIEXPORT jstring JNICALL Java_rolandomagico_jniresourceloader_ResourceLoader_getTranslatedString
+(JNIEnv* env, jclass instance, jstring library, jlong languageId, jlong menuId, jstring defaultValue)
 {
-  DWORD pdwType;
-  const char *nativeKey = env->GetStringUTFChars(key, FALSE);
-  const char *nativeLocation = env->GetStringUTFChars(location, FALSE);
-
-  LSTATUS status = RegGetValueA(((HKEY)(ULONG_PTR)((LONG)hKey)), nativeLocation, nativeKey, RRF_RT_ANY, &pdwType, buffer, bufferLength);
-
-  env->ReleaseStringUTFChars(key, nativeKey);
-  env->ReleaseStringUTFChars(location, nativeLocation);
-
-  return status;
+  return nullptr;
 }
-
-JNIEXPORT jlong JNICALL Java_rolandomagico_jniregistry_Registry_readLongValue(JNIEnv* env, jobject, jint hKey, jstring location, jstring key, jlong defaultValue)
-{
-	long buffer;
-	DWORD bufferLength = sizeof(buffer);
-	
-  if (ReadRegistry(env, hKey, location, key, &buffer, &bufferLength) == ERROR_SUCCESS)
-  {
-    return buffer;
-  }
-  else
-  {
-    return defaultValue;
-  }
-}
-
-JNIEXPORT jstring JNICALL Java_rolandomagico_jniregistry_Registry_readStringValue(JNIEnv* env, jobject, jint hKey, jstring location, jstring key, jstring defaultValue)
-{
-	char buffer[1024];
-	DWORD bufferLength = sizeof(buffer);
-
-  if (ReadRegistry(env, hKey, location, key, &buffer, &bufferLength) == ERROR_SUCCESS)
-	{
-		return env->NewStringUTF(buffer);
-	}
-	else
-	{
-		return defaultValue;
-	}
-}
-
-JNIEXPORT void JNICALL Java_rolandomagico_jniregistry_Registry_writeStringValue(JNIEnv* env, jobject, jint hKey, jstring location, jstring key, jstring value)
-{
-  const char* nativeKey = env->GetStringUTFChars(key, FALSE);
-  const char* nativeLocation = env->GetStringUTFChars(location, FALSE);
-  const char* nativeValue = env->GetStringUTFChars(value, FALSE);
-
-  HKEY localKey;
-
-  size_t size = strlen(nativeLocation) + 1;
-  wchar_t* nativeLocation_wc = new wchar_t[size];
-  size_t outSize;
-  mbstowcs_s(&outSize, nativeLocation_wc, size, nativeLocation, size - 1);
-  RegOpenKeyEx(((HKEY)(ULONG_PTR)((LONG)hKey)), nativeLocation_wc, 0, KEY_SET_VALUE, &localKey);
-  LSTATUS status = RegSetValueExA(localKey, nativeKey, NULL, REG_SZ, (const BYTE *)nativeValue, (DWORD)(strlen(nativeValue) + 1));
-  RegCloseKey(localKey);
-
-  env->ReleaseStringUTFChars(key, nativeKey);
-  env->ReleaseStringUTFChars(location, nativeLocation);
-  env->ReleaseStringUTFChars(value, nativeValue);
-}
-
 /***********************************************************************************************************************
  EOF
 ***********************************************************************************************************************/
